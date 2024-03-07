731fe231c7802ea379f75424317047c28c163eaf
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-03-06 22:43:58 -0800

    Update mapset.cpp

--- src/mapset.cpp
+++ src/mapset.cpp
@@ -1,1 +1,35 @@
+#include "mapset.hpp"
+
+std::string to_lowercase(const std::string& str) {
+    std::string lower_str;
+    std::transform(str.begin(), str.end(), std::back_inserter(lower_str), [](unsigned char c) { return std::tolower(c); });
+    return lower_str;
+}
+
+std::set<std::string> load_stopwords(std::istream& stopwords) {
+    std::set<std::string> stopwords_set;
+    std::string word;
+    while (stopwords >> word) {
+        stopwords_set.insert(to_lowercase(word));
+    }
+    return stopwords_set;
+}
+
+std::map<std::string, int> count_words(std::istream& document, const std::set<std::string>& stopwords) {
+    std::map<std::string, int> word_counts;
+    std::string word;
+    while (document >> word) {
+        word = to_lowercase(word);
+        if (stopwords.find(word) == stopwords.end()) {
+            ++word_counts[word];
+        }
+    }
+    return word_counts;
+}
+
+void output_word_counts(const std::map<std::string, int>& word_counts, std::ostream& output) {
+    for (const auto& [word, count] : word_counts) {
+        output << word << " " << count << '\n';
+    }
+}
 
