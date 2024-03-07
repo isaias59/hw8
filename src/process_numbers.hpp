dec5e4a01c434d8e813775769909c8433743ddc4
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-03-06 22:24:41 -0800

    Update process_numbers.hpp

--- src/process_numbers.hpp
+++ src/process_numbers.hpp
@@ -1,1 +1,15 @@
 
+#ifndef PROCESS_NUMBERS_HPP
+#define PROCESS_NUMBERS_HPP
+
+#include <vector>
+#include <iosfwd>
+#include <iostream>
+
+using namespace std;
+
+void read_numbers(std::istream& in, std::vector<int>& numbers);
+void write_odds(const std::vector<int>& numbers, std::ostream& out);
+void write_evens(const std::vector<int>& numbers, std::ostream& out);
+
+#endif // PROCESS_NUMBERS_HPP
